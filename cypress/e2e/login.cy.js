eb4f5f027a30c07e364fac7dc7ea42f2ce36b553
Amanda Ruiz <80833331+amandavruiz@users.noreply.github.com>
2025-07-29 23:20:52 -0300

    Adicionei dados do fixture e alterei o gitignore

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,19 +5,23 @@ describe('Testes de Login', () => {
   })
   it('Login com dados válidos deve permitir entrada no sistema', () => {
     // Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('123456')
-    cy.contains('button','Entrar').click()
+    cy.fixture('credenciais').then(credenciais => {
+      cy.get('#username').click().type(credenciais.valida.usuario)
+      cy.get('#senha').click().type(credenciais.valida.senha)
+    })
+    cy.contains('button', 'Entrar').click()
 
     //Assert
-    cy.contains('h4','Realizar Transferência').should('be.visible')
+    cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
 
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
     // Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('654321')
-    cy.contains('button','Entrar').click()
+    cy.fixture('credenciais').then(credenciais => {
+      cy.get('#username').click().type(credenciais.invalida.usuario)
+      cy.get('#senha').click().type(credenciais.invalida.senha)
+    })
+    cy.contains('button', 'Entrar').click()
 
     //Assert
     cy.get('.toast').should('be.visible').should('have.text', 'Erro no login. Tente novamente.')
